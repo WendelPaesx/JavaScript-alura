8a9760b5aaecb5ca48d1226cd902c599b49802d1
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-08-25 04:00:45 -0300

    update listas

--- 5-AtribuiçãoDeVariaveis.js
+++ 5-AtribuiçãoDeVariaveis.js
@@ -2,10 +2,10 @@
 
 console.log("trabalhando com atribuição de variaveis");
 
-const idade = 24;
-const primeiroNome = "Wendel";
-const sobrenome ="Paes"
 
+const primeiroNome = "Wendel";
+const sobrenome ="Paes";
+let idade;
 
 console.log(idade);
 console.log(primeiroNome);
@@ -16,7 +16,12 @@ console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 const nomeCompleto = primeiroNome +""+ sobrenome;
 console.log (nomeCompleto);
 
-let contador =0;
+let contador = 0;
 contador = contador +1;
+
+
+idade = 25;
+idade = idade+1;
+console.log(idade)
 
 
---
+++ lsitas.js
@@ -0,0 +1,17 @@
+console.log(`trabalhando com listas`);
+// const salvador =`Salvador`;
+// const sãoPaulo =`São Paulo`;
+// const riodeJaneiro =`Rio de Janeiro`;
+
+
+const listasDeDestinos = new Array(
+    `Salvador`, 
+    `São Paulo`,
+    `Rio de Janeiro`,
+    
+
+);
+ listasDeDestinos.push(`curitiba`) // adicionando um item na listasDeDestinos
+console.log("Destinos possiveis");
+console.log(listasDeDestinos)
+
