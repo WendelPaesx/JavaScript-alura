b4a4bce32a3c3b52b55ea664ba38adaa9faaa9df
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-08-25 02:46:15 -0300

    Update 1-MeuPrimeiroPrograma.js

--- 1-MeuPrimeiroPrograma.js
+++ 1-MeuPrimeiroPrograma.js
@@ -1,1 +1,14 @@
-console.log("Meu primeiro programa");
+console.log("Meu primeiro programa: trabalhando com variaveis");
+const idade= 24;
+
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const IdadeSomada = idade+3;
+console.log(IdadeSomada);
+
+
