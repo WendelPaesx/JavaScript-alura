ce04d8cf506739b20bf5214793efa77a280a0b4c
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-08-30 02:12:35 -0300

    7 condicionais

---
+++ 7-condicionais.js
@@ -0,0 +1,20 @@
+console.log(`trabalhando com condicionais`);
+
+
+const listasDeDestinos = new Array(
+    `Salvador`, 
+    `São Paulo`,
+    `Rio de Janeiro`,
+    
+
+);
+ listasDeDestinos.push(`curitiba`);// adicionando um item na listasDeDestinos
+console.log("Destinos possiveis");
+console.log(listasDeDestinos);
+
+listasDeDestinos.splice(1,1); 
+console.log(listasDeDestinos);
+
+
+
+
