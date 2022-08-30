b976f82a85f8c402a959fbab6026c333130a26c7
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2022-08-30 03:30:18 -0300

    Update 7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -2,18 +2,55 @@ console.log(`trabalhando com condicionais`);
 
 
 const listasDeDestinos = new Array(
-    `Salvador`, 
+    `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`,
-    
+
 
 );
- listasDeDestinos.push(`curitiba`);// adicionando um item na listasDeDestinos
+
+const idadeComprador = 19;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
+
 console.log("Destinos possiveis");
 console.log(listasDeDestinos);
 
-listasDeDestinos.splice(1,1); 
+// if (idadeComprador >= 18) {
+
+//     console.log("comprador maior de idade");
+//     listasDeDestinos.splice(1, 1);
+// } else if (estaAcompanhada) {
+//     console.log("comprador está acompanhado");
+//     listasDeDestinos.splice(1, 1);
+
+// } else {
+//     console.log("Comprador nao é maior de idade, não posso vender!");
+
+// }
+
+if ( idadeComprador >= 18 || estaAcompanhada == true
+    ) {
+
+    console.log("Autorizado!");
+    listasDeDestinos.splice(2, 1);
+}  else {
+    console.log("Comprador nao é maior de idade, não posso vender!");
+
+}
+
+console.log("Embarque: \n\n")
+if(idadeComprador > 18 && temPassagemComprada){
+    console.log("Boa Viagem!")
+}else{
+    console.log("Nao autorizado!")
+}
+
+
+
 console.log(listasDeDestinos);
+
+
 
 
 
